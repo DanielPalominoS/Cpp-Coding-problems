e61b4ea59bbceaf8ea37a27cdc9f91216ab841a8
Daniel Palomino <daniel.santiago918@hotmail.com>
2024-03-17 20:04:22 -0500

    add largest odd number

---
+++ largest_ood_number/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string copy=num;
+        for (int i=0;i<num.length();i++){
+            string copy=num;
+            if ((stoi(copy)%2)!=0){
+
+            }
+        }
+        return "";    
+    }
+};
+int main(int, char**){
+    cout << "Hello, from lon!\n";
+}
